5cba81e75c59f5fe467eef9743325e3778677f51
zhushilai <zsl19900608@163.com>
2020-08-19 17:22:49 +0800

    测试properties的返回json数据

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domian.ServiceSetting;
+
+@RestController
+public class TestController {
+
+	@Autowired
+	private ServiceSetting  serviceSetting;
+	
+	@GetMapping("/v1/test_properties")
+	public Object testProperties() {
+		
+		return serviceSetting;
+	}
+	
+	
+}
